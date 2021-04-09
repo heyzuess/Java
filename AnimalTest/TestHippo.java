c481dbe1e8848ad41d8a7c376b2e31d88277b8cb
Jesse Iberri <jiberri@localhost.localdomain>
2021-04-08 22:37:19 -0700

    Added super constuctor and super constructor with parameters examples

---
+++ AnimalTest/TestHippo.java
@@ -0,0 +1,7 @@
+public class TestHippo {
+    public static void main (String[] args) {
+        System.out.println("Starting...");
+        Animal a = new Animal();
+        Hippo h = new Hippo();
+    }
+}
---
+++ AnimalTest2/MakeHippo.java
@@ -0,0 +1,6 @@
+public class MakeHippo {
+    public static void main (String[] args) {
+        Hippo myHippo = new Hippo("Buffy");
+        System.out.println(myHippo.getName());
+    }
+}
